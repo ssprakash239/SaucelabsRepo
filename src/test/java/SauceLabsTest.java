d3449b86b771fd14b9a26e5f2e05888ef481f68c
ssprakash239 <ssprakashrampalli@gmail.com>
2021-03-18 18:17:05 +0530

    Updated with SauceLabs Projet with sample test case

---
+++ src/test/java/SauceLabsTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+public class SauceLabsTest {
+    public static final String USERNAME = "ssprakashrampalli";
+    public static final String ACCESS_KEY = "d3680808-f5c5-4685-890f-ea323c85d921";
+    public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+
+    public static void main(String[] args) throws Exception {
+
+        DesiredCapabilities caps = DesiredCapabilities.chrome();
+        caps.setCapability("platform", "Windows 10");
+        caps.setCapability("version", "latest");
+
+        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+        driver.get("http://www.google.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+
+}}
